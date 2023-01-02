466a82cd1bb0b5e822035e0a5bd71323087c0a4a
unknown <36856354+azharpathil@users.noreply.github.com>
2023-01-02 17:59:54 +0400

    updated

---
+++ DataServeDB/file_fuzz_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func FuzzFile(f *testing.F) {
+
+	for _, seed := range testCasefileArray {
+		f.Add(seed.method, seed.path, seed.filename)
+	}
+	f.Fuzz(func(t *testing.T, method, path, body string) {
+		if method == "POST" {
+			successResult, err := restApiCallMu(method, path, body)
+			if err != nil {
+				t.Fatalf("%v\n", err)
+			} else {
+				log.Println(successResult)
+			}
+		} else {
+			successResult, err := restApiCall(method, path, body)
+			if err != nil {
+				t.Fatalf("%v\n", err)
+			} else {
+				log.Println(successResult)
+			}
+		}
+	})
+}
--- DataServeDB/tables_fuzz_test.go
+++ DataServeDB/tables_fuzz_test.go
@@ -11,12 +11,9 @@ Description: Fuzz tests for table operations.
 TODO: Devise useful fuzz test cases.
 */
 
-func FuzzHex(f *testing.F) {
-	for i, seed := range testCaseArray {
+func FuzzTable(f *testing.F) {
+	for _, seed := range testCaseArray {
 		f.Add(seed.method, seed.path, seed.body)
-		if i == 1000 {
-			break
-		}
 	}
 	f.Fuzz(func(t *testing.T, method, path, body string) {
 		successResult, err := restApiCall(method, path, body)
